316e2f9da8fe3bc1889418b26e7ccfb0428d540c
zIlgar <ilqarshikhverdiyev37@gmail.com>
2023-09-02 08:32:30 +0400

    Arduino esc code added

--- telemetry_project_ard/telemetry_project/telemetry_project.ino
+++ telemetry_project_ard/telemetry_project/telemetry_project.ino
@@ -1,10 +1,12 @@
+# include <Adafruit_MPU6050.h>
+# include <Adafruit_Sensor.h>
 # include <SoftwareSerial.h>
-#include <Servo.h>
+# include <Servo.h>
+# include <Wire.h>
 # include "DHT.h"
 
 # define DHTTYPE DHT11
-# define POT A0
-
+# define POT A2
 
 enum HCSR04_PIN {
     TRIG = 3,
@@ -12,9 +14,11 @@ enum HCSR04_PIN {
 };
 
 Servo servo;
+Servo esc;
 
 const int DHT11_PIN = 11;
-const int SERVO_PIN = 13;
+const int SERVO_PIN = 12;
+const int ESC_PIN = 8;
 
 DHT dht(DHT11_PIN, DHTTYPE);
 
@@ -42,9 +46,14 @@ short comp_ang, m_comp_ang;
 short ultra, humid, temp;
 short servo_ang;
 
+int pot_value;
+
 String msg = "";
 
 SoftwareSerial ss(19, 18);
+
+Adafruit_MPU6050 mpu;
+double gyro_value = 0;
 
 void readDHT11() {
     humidity = dht.readHumidity() * 100;
@@ -52,8 +61,6 @@ void readDHT11() {
 }
 
 void readHCSR04() {
-    servo.attach(SERVO_PIN);
-
     digitalWrite(TRIG, LOW);
     delayMicroseconds(2);
 
@@ -140,10 +147,37 @@ void setup() {
     send_msg(buf, 8);
 
     req_type = "CUR_DATA";
+
+    servo.attach(SERVO_PIN);
+    esc.attach(ESC_PIN)
+
+    while (!mpu.begin()) {
+        delay(10);
+    }
+
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 }
 
 void loop() {
-    delay(2000);
+    pot_value = analogRead(POT);
+    pot_value = map(pot_value, 0, 1023, 0, 180);
+
+    esc.write(pot_value)
+
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    gyro_value += g.gyro.z;
+
+    Serial.println(gyro_value * 24);
+
+    if (gyro_value > 0)
+        gyro_value = gyro_value - ((int) gyro_value / 60)  * 60;
+    else
+        gyro_value = gyro_value - ((int) gyro_value / - 60) * - 60;
+
+    delay(100);
 
     readDHT11();
     readHCSR04();
@@ -167,8 +201,11 @@ void loop() {
         Serial.print(msg);
     }
 
-    comp_ang = analogRead(POT);
+    comp_ang = gyro_value * 24;
     m_comp_ang = 90;
+
+    servo.write(45 + min(max(gyro_value * - 24, - 45), 45));
+    Serial.println(45 + min(max(gyro_value * - 24, - 45), 45));
 
     sprintf(buf, cur_req, year_s, month_s, day_s, hour_s, minute_s, second_s, req_type, req_id, dev_id, gps_w, gps_f_s, x_, y_, m_x_, m_y_, dis, comp_ang, m_comp_ang, distance, humidity, temperature, servo_ang);
 
