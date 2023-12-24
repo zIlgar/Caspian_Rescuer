4d046d6f28fbb71c95b775c19c9d722a7b8edf0c
zIlgar <66477266+zIlgar@users.noreply.github.com>
2023-12-24 23:17:39 +0400

    Add files via upload

--- telemetry_project/telemetry_project/api/logs/clear.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-everything = list(Path('api/logs').iterdir())
-
-for something in everything:
-    if something.name != 'clear.py':
-        something.unlink(missing_ok = True)
--- telemetry_project/telemetry_project/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Device
-
-class DeviceSerializer(ModelSerializer):
-    class Meta:
-        model = Device
-        fields = '__all__'
